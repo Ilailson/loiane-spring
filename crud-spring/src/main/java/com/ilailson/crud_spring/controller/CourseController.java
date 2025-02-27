f907830fd2f050b8d65dac5f9d475172417a6917
ilailsonrocha <ilailsonrocha@iec.gov.br>
2025-02-27 15:54:34 -0300

    39: Adicionando Camada de Serviço na API

--- crud-spring/src/main/java/com/ilailson/crud_spring/controller/CourseController.java
+++ crud-spring/src/main/java/com/ilailson/crud_spring/controller/CourseController.java
@@ -12,81 +12,63 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ilailson.crud_spring.model.Course;
-import com.ilailson.crud_spring.repository.CourseRepository;
+import com.ilailson.crud_spring.service.CourseService;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import lombok.AllArgsConstructor;
 
 @Validated
 @RestController
 @RequestMapping("/api/courses")
-@AllArgsConstructor
 public class CourseController {
 
-    private final CourseRepository courseRepository;
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
 
 
+    @GetMapping // utilizado para visualizar só os ativos da deleção lógica com hibernate // +
+    public @ResponseBody List<Course> list(){
+        return courseService.list();
+    }
 
-    @GetMapping("/{id}")// utilizado para visualizar só os ativos da deleção lógica com hibernate 
+    @GetMapping("/{id}")// utilizado para visualizar só os ativos da deleção lógica com hibernate // +
     public ResponseEntity<Course> findById(@PathVariable @NotNull @Positive Long id) {
-        return courseRepository
+        return courseService
                 .findById(id)
                 .map(record -> ResponseEntity.ok(record))
                 .orElse(ResponseEntity.notFound().build());
       }
 
-    // public CourseController(CourseRepository courseRepository) {
-    //     this.courseRepository = courseRepository;
-    // } mesma coisa @AllArgsConstructor
-
-    // @RequestMapping(method = RequestMethod.GET)
-    @GetMapping // utilizado para visualizar só os ativos da deleção lógica com hibernate 
-    public List<Course> list(){
-        return courseRepository.findAll();
-    }
-
-    // @RequestMapping(method =  RequestMethod.POST)
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Course create(@RequestBody @Valid Course course) {
-        return courseRepository.save(course);
-        // System.out.println(course.getName());
-        // return ResponseEntity
-        //         .status(HttpStatus.CREATED)
-        //         .body(courseRepository.save(course));
-
-        // @Valid. Garantir envio de dados corretos de acordo com a validação do modelo
+        return courseService.create(course);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Course> update(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid Course course) {
-        return courseRepository
-                .findById(id) // Usando a instância correta
-                .map(recordFound -> {
-                    recordFound.setName(course.getName());
-                    recordFound.setCategory(course.getCategory());
-                    Course updated = courseRepository.save(recordFound);
-                    return ResponseEntity.ok(updated);
-                })
+        return courseService.update(id, course)
+                .map(recordFound -> ResponseEntity.ok().body(recordFound))
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @DeleteMapping("/{id}") // utilizado na deleção lógica com hibernate 
+    @DeleteMapping("/{id}") // utilizado na deleção lógica com hibernate // +
     public ResponseEntity<Void>  delete(@PathVariable @NotNull @Positive Long id){
-        return courseRepository
-                .findById(id) // Usando a instância correta
-                .map(recordFound -> {
-                    courseRepository.deleteById(id);
-                    return ResponseEntity.noContent().<Void>build();
-                })
-                .orElse(ResponseEntity.notFound().build());
+        if(courseService.delete(id)){
 
+            return ResponseEntity.noContent().<Void>build();
+        }
+        else
+            return ResponseEntity.notFound().build();
     }
 
 }
